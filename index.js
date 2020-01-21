1fd89aeb8a1c6f8687e72ae0b2f12b656ccf677f
vuhi <vuhi@mail.uc.edu>
2020-01-21 01:01:32 -0500

    global error handler

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+require('express-async-errors');
 const cors = require('cors');
 const morgan = require('morgan');
 const dbConnect = require('./db/db.connection');
+const errorHandler = require('./utils/middlewares/error-handler.middleware');
 const { PORT, CONNECTION_STRING } = require('./utils/config');
 
 const recipeRoutes = require('./routes/recipe.routes');
@@ -15,5 +17,8 @@ app.use(morgan('dev'));
 dbConnect(CONNECTION_STRING);
 
 app.use('/api/recipes', recipeRoutes);
+
+// GLOBAL ERROR HANDLER, SHOULD BE PLACE AT THE END
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running in [${process.env.NODE_ENV}] mode on port: ${PORT}`));
