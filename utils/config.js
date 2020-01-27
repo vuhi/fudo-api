f7d691e69e3ce36ad4ef7f7a8012db75528f604e
vuhi <vuhi@mail.uc.edu>
2020-01-26 21:58:02 -0500

    add "STATUSES" + "ROLES"

--- utils/config.js
+++ utils/config.js
@@ -4,3 +4,5 @@ dotenv.config({ path: './config.env' });
 
 module.exports.PORT = process.env.PORT || 3000;
 module.exports.CONNECTION_STRING = process.env.CONNECTION_STRING;
+module.exports.STATUSES = process.env.STATUSES.split(',');
+module.exports.ROLES = process.env.ROLES.split(',');
