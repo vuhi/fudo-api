4def104002a0b96825efbd262d256eefe1a69491
vuhi <vuhi@mail.uc.edu>
2020-01-26 21:58:31 -0500

    handle Mongo unique

--- utils/middlewares/error-handler.middleware.js
+++ utils/middlewares/error-handler.middleware.js
@@ -1,9 +1,16 @@
 module.exports = async (err, req, res, next) => {
-  res.status(err.statusCode || 500);
+  let { message, statusCode, originalError } = err;
+  if (err.name === 'MongoError' && err.code === 11000) {
+    message = `${Object.getOwnPropertyNames(err.keyValue)[0]} has been used, please provide different value`;
+    statusCode = 400;
+    originalError = err;
+  }
+
+  res.status(statusCode || 500);
   res.json({
     status: 'error thrown!',
-    message: err.message,
-    data: err.originalError || null
+    message: message,
+    data: process.env.NODE_ENV === 'production' ? null : originalError
   });
   next();
 };
