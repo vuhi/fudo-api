c07e0d204dc5af156c072520f5398397c55f2052
vuhi <vuhi@mail.uc.edu>
2020-01-26 22:00:13 -0500

    remove old files

--- utils/index.js
+++
@@ -1,5 +0,0 @@
-const { ErrorResponse } = require('./error-response.class');
-const { resFun } = require('./success-response.func');
-const { isObjectNullOrEmpty } = require('./helpers.func');
-
-module.exports = { ErrorResponse, resFun, isObjectNullOrEmpty };
