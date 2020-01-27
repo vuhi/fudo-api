f50729d08bcd424f3812fe39ce27229c1ed6320a
vuhi <vuhi@mail.uc.edu>
2020-01-26 21:57:24 -0500

    refactor routes -> file

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
 const express = require('express');
-require('express-async-errors');
-const cors = require('cors');
-const morgan = require('morgan');
-const dbConnect = require('./db/db.connection');
-const errorHandler = require('./utils/middlewares/error-handler.middleware');
 const { PORT, CONNECTION_STRING } = require('./utils/config');
-
-const recipeRoutes = require('./routes/recipe.routes');
+require('express-async-errors');
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(morgan('dev'));
 
-dbConnect(CONNECTION_STRING);
+// SET UP ROUTES
+require('./routes/routes')(app);
 
-app.use('/api/recipes', recipeRoutes);
-
-// GLOBAL ERROR HANDLER, SHOULD BE PLACE AT THE END
-app.use(errorHandler);
+// SET UP DB CONNECTION
+require('./db/db.connection')(CONNECTION_STRING);
 
 app.listen(PORT, () => console.log(`Server running in [${process.env.NODE_ENV}] mode on port: ${PORT}`));
