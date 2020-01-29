6cc01201f8163271b20a434331544ef18d7a60f8
vuhi <vuhi@mail.uc.edu>
2020-01-29 01:53:37 -0500

    hot fixes

--- utils/config.js
+++ utils/config.js
@@ -4,5 +4,27 @@ dotenv.config({ path: './config.env' });
 
 module.exports.PORT = process.env.PORT || 3000;
 module.exports.CONNECTION_STRING = process.env.CONNECTION_STRING;
-module.exports.STATUSES = process.env.STATUSES.split(',');
-module.exports.ROLES = process.env.ROLES.split(',');
+
+const ACTIVE = 'ACTIVE';
+const BLOCKED = 'BLOCKED';
+module.exports.ACTIVE = ACTIVE;
+module.exports.BLOCKED = BLOCKED;
+module.exports.STATUSES = [ACTIVE, BLOCKED];
+
+const USER = 'USER';
+const ADMIN = 'ADMIN';
+const SUPER_ADMIN = 'SUPER_ADMIN';
+module.exports.USER = USER;
+module.exports.ADMIN = ADMIN;
+module.exports.SUPER_ADMIN = SUPER_ADMIN;
+module.exports.ROLES = [USER, ADMIN, SUPER_ADMIN];
+
+const EASY = 'EASY';
+const MEDIUM = 'MEDIUM';
+const HARD = 'HARD';
+const CHEF = 'CHEF';
+module.exports.EASY = EASY;
+module.exports.MEDIUM = MEDIUM;
+module.exports.HARD = HARD;
+module.exports.CHEF = CHEF;
+module.exports.LEVELS = [EASY, MEDIUM, HARD, CHEF];
